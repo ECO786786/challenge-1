549346e21988a1c6590990737ffdc9132331262c
Emma Collins <lowpee56@hotmail.com>
2025-01-19 22:26:00 +0000

    add fizz buzz test

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-//Write your function here
+const fizz_buzz = (number) => {
+  const fizz_buzz_array = [];
+  if (number % 3 === 0 && number % 5 === 0) {
+    fizz_buzz_array.push("FizzBuzz");
+  } else if (number % 3 === 0) {
+    fizz_buzz_array.push("Fizz");
+  } else if (number % 5 === 0) {
+    fizz_buzz_array.push("Buzz");
+  } else {
+    fizz_buzz_array.push(number);
+  }
+  return fizz_buzz_array;
+};
+
+module.exports = fizz_buzz;
